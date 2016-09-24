bb958af638afc8033fa69f427667bbca08936046
Fondson Tran <fondsont@gmail.com>
2016-09-24 14:40:05 -0400

    not working current

--- app/src/main/java/hackuweather/lockuweather/MainActivity.java
+++ app/src/main/java/hackuweather/lockuweather/MainActivity.java
@@ -15,11 +15,13 @@ import android.util.Log;
 import android.widget.Toast;
 
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 
+import hackuweather.lockuweather.Weather.Current;
 import hackuweather.lockuweather.Weather.Forecast;
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -33,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
     private static long LOCATION_REFRESH_TIME = 2000;
     private static final String TAG = MainActivity.class.getSimpleName();
     private Forecast mForecast;
+    private static final int NETWORK_PERM_CODE = 0;
+    private String[] NETWORK_PERM = {"android.permission.ACCESS_NETWORK_STATE","android.permission.INTERNET"};
 
     private LocationManager mLocationManager;
     private LocationListener mLocationListener;
@@ -44,6 +48,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M
+                && checkCallingOrSelfPermission("android.permission.ACCESS_NETWORK_STATE")!= PackageManager.PERMISSION_GRANTED
+                && checkCallingOrSelfPermission("android.permission.INTERNET")!= PackageManager.PERMISSION_GRANTED){
+            requestPermissions(NETWORK_PERM, NETWORK_PERM_CODE);
+        }
+
+        getCurrent();
         // initialize a location listener
         mLocationListener = new LocationListener() {
             @Override
@@ -70,6 +81,85 @@ public class MainActivity extends AppCompatActivity {
         };
     }
 
+    private void getCurrent() {
+        // initialize the api with key and paramters
+        String apiKey = "hackuweather2016";
+        String forecastURL = "http://apidev.accuweather.com/currentconditions/v1/"+ "335315" + ".json?apikey=" + apiKey + "/";
+
+        if (isNetworkAvailable()) {
+            // display refresh animation (i.e. make the animation spin)
+            toggleRefresh();
+            // connect to the api using ok http
+            OkHttpClient okHttpClient = new OkHttpClient();
+            Request request = new Request.Builder().url(forecastURL).build();
+            Call call = okHttpClient.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            // turn the animation off
+                            toggleRefresh();
+                        }
+                    });
+                    // display an error message to the user
+                    alertUserAboutError();
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    // if a valid response is relieved
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            // turn off the refresh animation
+                            toggleRefresh();
+                        }
+                    });
+                    try {
+                        // store the data from the response
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
+                        // can't remember what this does
+                        if (response.isSuccessful()) {
+                            // update the forecast with the details from the
+                            mForecast.setCurrent(getCurrentDetails(jsonData));
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    //updateDisplay();
+                                }
+                            });
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "COULD NOT CONNECT TO FORECAST API");
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(this, R.string.toast_unavailable_network_message,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private Current getCurrentDetails(String jsonData) {
+        Current current = new Current();
+        try {
+            JSONObject jsonObject = new JSONObject(jsonData);
+            JSONObject tempJson = jsonObject.getJSONObject("Temperature");
+            JSONObject metricJson = tempJson.getJSONObject("Metric");
+
+            Log.d("current", Integer.toString(metricJson.getInt("Value")));
+            current.setTemperature(jsonObject.getInt(""));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return current;
+    }
 
     private void getForecast(double latitude, double longitude) {
         // initialize the api with key and paramters
@@ -111,15 +201,15 @@ public class MainActivity extends AppCompatActivity {
                     try {
                         // store the data from the response
                         String jsonData = response.body().string();
-                        Log.v(TAG, response.body().string());
+                        //Log.v(TAG, response.body().string());
                         // can't remember what this does
                         if (response.isSuccessful()) {
                             // update the forecast with the details from the
-                            mForecast = parseForecastDetails(jsonData);
+                            //mForecast = parseForecastDetails(jsonData);
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    updateDisplay();
+                                    //updateDisplay();
                                 }
                             });
                         } else {
@@ -127,8 +217,6 @@ public class MainActivity extends AppCompatActivity {
                         }
                     } catch (IOException e) {
                         Log.e(TAG, "COULD NOT CONNECT TO FORECAST API");
-                    } catch (JSONException e) {
-                        Log.e(TAG, "COULD NOT GET CURRENT WEATHER DATA FROM JSON");
                     }
                 }
             });
@@ -143,15 +231,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void toggleRefresh() {
 
-    }
-
-    private Forecast parseForecastDetails(String jsonData) throws JSONException {
-        Forecast forecast = new Forecast();
-        JSONObject jsonForecast = new JSONObject(jsonData);
-        forecast.setCurrent(getCurrentDetails(jsonForecast, timezone));
-        forecast.setHourlyForecast(getHourlyForecast(jsonForecast, timezone));
-        forecast.setDailyForecast(getDailyForecast(jsonForecast, timezone));
-        return forecast;
     }
 
 
@@ -176,5 +255,18 @@ public class MainActivity extends AppCompatActivity {
 
         mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
                 LOCATION_REFRESH_DISTANCE, mLocationListener);
+    }
+
+    //handles permission requests
+    @Override
+    public void onRequestPermissionsResult(int permsRequestCode, String[] permissions,int[] grantResults){
+        switch (permsRequestCode){
+            case NETWORK_PERM_CODE:
+                if (grantResults.length>=0
+                        && grantResults[0]==PackageManager.PERMISSION_GRANTED) {
+
+                }
+                break;
+        }
     }
 }
