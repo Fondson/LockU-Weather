15fe9e4d493b2debed74c712233c214eacdce50f
Mark Emery <mark.e.emery@gmail.com>
2016-09-24 18:36:42 -0400

    Merge remote-tracking branch 'origin/weather'
    # Conflicts:
    #	.idea/libraries/animated_vector_drawable_24_2_0.xml
    #	.idea/libraries/appcompat_v7_24_2_0.xml
    #	.idea/libraries/play_services_location_9_6_0.xml
    #	.idea/libraries/support_annotations_24_2_0.xml
    #	.idea/libraries/support_compat_24_2_0.xml
    #	.idea/libraries/support_core_ui_24_2_0.xml
    #	.idea/libraries/support_core_utils_24_2_0.xml
    #	.idea/libraries/support_fragment_24_2_0.xml
    #	.idea/libraries/support_media_compat_24_2_0.xml
    #	.idea/libraries/support_vector_drawable_24_2_0.xml
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/hackuweather/lockuweather/MainActivity.java

--- app/src/main/java/hackuweather/lockuweather/Weather/Forecast.java
+++ app/src/main/java/hackuweather/lockuweather/Weather/Forecast.java
@@ -32,43 +32,39 @@ public class Forecast {
             mDailyForecast = dailyForecast;
         }
 
-        public static int getIconId(String iconString) {
+        public static int getIconId(int iconString) {
             int iconId = R.drawable.weather_icon1;
 
-            if (Integer.parseInt(iconString) <=2 || Integer.parseInt(iconString) == 30) {
+            if (iconString <=2 || iconString == 30) {
                 iconId = R.drawable.weather_icon1;
             }
-            else if (Integer.parseInt(iconString) >= 3 && Integer.parseInt(iconString) <= 6) {
+            else if ((iconString) >= 3 && (iconString) <= 6) {
                 iconId = R.drawable.weather_icon4;
             }
-            else if (Integer.parseInt(iconString) >= 7 && Integer.parseInt(iconString) <= 8 ||
-                    Integer.parseInt(iconString) == 32) {
+            else if ((iconString) >= 7 && (iconString) <= 8 || (iconString) == 32) {
                 iconId = R.drawable.weather_icon3;
             }
-            else if (Integer.parseInt(iconString) >= 9 && Integer.parseInt(iconString) <= 11 ||
-                    Integer.parseInt(iconString) == 31) {
+            else if ((iconString) >= 9 && (iconString) <= 11 || (iconString) == 31) {
                 iconId = R.drawable.weather_icon7;
             }
-            else if (Integer.parseInt(iconString) == 12 || Integer.parseInt(iconString) == 18 ||
-                    Integer.parseInt(iconString) == 39 || Integer.parseInt(iconString) == 40) {
+            else if ((iconString) == 12 || (iconString) == 18 || (iconString) == 39 ||
+                    (iconString) == 40) {
                 iconId = R.drawable.weather_icon5;
             }
-            else if (Integer.parseInt(iconString) >= 13 && Integer.parseInt(iconString) <= 17 &&
-                    Integer.parseInt(iconString) != 15) {
+            else if ((iconString) >= 13 && (iconString) <= 17 && (iconString) != 15) {
                 iconId = R.drawable.weather_icon2;
             }
-            else if (Integer.parseInt(iconString) == 15 || Integer.parseInt(iconString) == 41 ||
-                    Integer.parseInt(iconString) == 42) {
+            else if ((iconString) == 15 || (iconString) == 41 || (iconString) == 42) {
                 iconId = R.drawable.weather_icon9;
             }
-            else if (Integer.parseInt(iconString) >= 19 && Integer.parseInt(iconString) <= 25 ||
-                    Integer.parseInt(iconString) == 43 || Integer.parseInt(iconString) == 44) {
+            else if ((iconString) >= 19 && (iconString) <= 25 || (iconString) == 43 ||
+                    (iconString) == 44) {
                 iconId = R.drawable.weather_icon10;
             }
-            else if (Integer.parseInt(iconString) >= 26 && Integer.parseInt(iconString) <= 29) {
+            else if ((iconString) >= 26 && (iconString) <= 29) {
                 iconId = R.drawable.weather_icon11;
             }
-            else if (Integer.parseInt(iconString) >= 33 && Integer.parseInt(iconString) <= 38) {
+            else if ((iconString) >= 33 && (iconString) <= 38) {
                 iconId = R.drawable.weather_icon8;
             }
             return iconId;
