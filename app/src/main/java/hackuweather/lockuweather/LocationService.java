33c6441269240fc6280d31504dadfdb09fbfa6a2
Fondson Tran <fondsont@gmail.com>
2016-09-24 19:18:31 -0400

    fixed clock format bug

--- app/src/main/java/hackuweather/lockuweather/LocationService.java
+++ app/src/main/java/hackuweather/lockuweather/LocationService.java
@@ -1,6 +1,7 @@
 package hackuweather.lockuweather;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
