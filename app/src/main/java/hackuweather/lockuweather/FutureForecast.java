9cc733ba53eb60518a012e9be04942c8a929df45
Mark Emery <mark.e.emery@gmail.com>
2016-09-25 10:42:59 -0400

    Merge remote-tracking branch 'origin/weather'
    # Conflicts:
    #	.idea/libraries/animated_vector_drawable_24_2_0.xml
    #	.idea/libraries/appcompat_v7_24_2_0.xml
    #	.idea/libraries/play_services_location_9_6_0.xml
    #	.idea/libraries/support_annotations_24_2_0.xml
    #	.idea/libraries/support_compat_24_2_0.xml
    #	.idea/libraries/support_core_ui_24_2_0.xml
    #	.idea/libraries/support_core_utils_24_2_0.xml
    #	.idea/libraries/support_fragment_24_2_0.xml
    #	.idea/libraries/support_media_compat_24_2_0.xml
    #	.idea/libraries/support_vector_drawable_24_2_0.xml
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/hackuweather/lockuweather/MainActivity.java

--- app/src/main/java/hackuweather/lockuweather/FutureForecast.java
+++ app/src/main/java/hackuweather/lockuweather/FutureForecast.java
@@ -91,19 +91,19 @@ public class FutureForecast extends AppCompatActivity {
     }
 
 
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            futureView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        }
-    }
+//    @Override
+//    public void onWindowFocusChanged(boolean hasFocus) {
+//        super.onWindowFocusChanged(hasFocus);
+//        if (hasFocus) {
+//            futureView.setSystemUiVisibility(
+//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+//        }
+//    }
 
 
     @Override
